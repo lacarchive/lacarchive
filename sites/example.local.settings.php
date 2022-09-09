db832e33916c9791ade3c6b567668bce333c4b28
Andrew Chernauskas <andrew@abstractpenguin.com>
2022-09-09 14:51:27 -0400

    Local dev settings

---
+++ sites/example.local.settings.php
@@ -0,0 +1,11 @@
+<?php 
+
+$databases['default']['default'] = array(
+  'driver' => 'mysql',
+  'host' => 'database',
+  'username' => 'drupal7',
+  'password' => 'drupal7',
+  'database' => 'drupal7',
+  'prefix' => '',
+  'port' => '3306',
+);
